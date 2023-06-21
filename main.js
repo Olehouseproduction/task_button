9c04710f228aa95a6cf9d556122e2154d575e798
Denzel <lebedkin112@gmail.com>
2023-06-21 21:46:18 +0700

    fix function

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ let background = document.querySelector("body");
 /*Несмотря на то, что NodeList не является массивом ( Array ), его вполне возможно перебрать при помощи метода forEach(). NodeList также можно конвертировать в Array при помощи Array.from()*/
 
 function turnsRed() {
-  nine.classList.toggle("active");
+  nine.classList.add("active");
   nine.classList.remove("delpink");
 
   // nine.classList.toggle("delpink");
@@ -70,10 +70,11 @@ function turnsPurple() {
 function random() {
   elementList = document.querySelectorAll(".only");
   let rand = Math.floor(Math.random() * elementList.length);
+
+  console.log(elementList[rand]);
   return elementList[rand];
   // elementList.splice(rand, 1);
 }
-console.log(elementList[rand]);
 
 function turnsBlue() {
   let randomButton = random();
@@ -100,48 +101,59 @@ function pulsation() {
 }
 
 function dropping() {
-  let randomButton = random();
+  let i = 0
+  let allButton = document.querySelectorAll(".surprise")
+  allButton.forEach((item)=>{
+    console.log(item, "каждый елемент ", i++)
+    item.classList.remove("active")
+    item.classList.remove("green")
+    item.classList.remove("blue")
+    item.classList.remove("pulse")
+    item.classList.remove("delpink")
+  })
 
-  five.classList.remove("active");
-  one.classList.remove("green");
-  four.classList.remove("green");
-  background.classList.remove("active");
-  three.classList.remove("active");
-  nine.classList.remove("active");
-  nine.classList.remove("pulse");
-  // console.log(randomButton);
-  randomButton.classList.remove("blue");
-  randomButton.classList.remove("pulse");
-  one.classList.remove("blue");
-  two.classList.remove("blue");
-  three.classList.remove("blue");
-  four.classList.remove("blue");
-  five.classList.remove("blue");
-  six.classList.remove("blue");
-  seven.classList.remove("blue");
-  eight.classList.remove("blue");
-  nine.classList.remove("blue");
-  refreshButton.classList.remove("blue");
+  // let randomButton = random();
 
-  one.classList.remove("pulse");
-  two.classList.remove("pulse");
-  three.classList.remove("pulse");
-  four.classList.remove("pulse");
-  five.classList.remove("pulse");
-  six.classList.remove("pulse");
-  seven.classList.remove("pulse");
-  eight.classList.remove("pulse");
-  nine.classList.remove("pulse");
-  refreshButton.classList.remove("pulse");
+  // five.classList.remove("active");
+  // one.classList.remove("green");
+  // four.classList.remove("green");
+  // background.classList.remove("active");
+  // three.classList.remove("active");
+  // nine.classList.remove("active");
+  // nine.classList.remove("pulse");
+  // // console.log(randomButton);
+  // randomButton.classList.remove("blue");
+  // randomButton.classList.remove("pulse");
+  // one.classList.remove("blue");
+  // two.classList.remove("blue");
+  // three.classList.remove("blue");
+  // four.classList.remove("blue");
+  // five.classList.remove("blue");
+  // six.classList.remove("blue");
+  // seven.classList.remove("blue");
+  // eight.classList.remove("blue");
+  // nine.classList.remove("blue");
+  // refreshButton.classList.remove("blue");
 
-  one.classList.remove("delpink");
-  two.classList.remove("delpink");
-  three.classList.remove("delpink");
-  four.classList.remove("delpink");
-  five.classList.remove("delpink");
-  six.classList.remove("delpink");
-  seven.classList.remove("delpink");
-  eight.classList.remove("delpink");
-  nine.classList.remove("delpink");
-  four.classList.remove("invisible");
+  // one.classList.remove("pulse");
+  // two.classList.remove("pulse");
+  // three.classList.remove("pulse");
+  // four.classList.remove("pulse");
+  // five.classList.remove("pulse");
+  // six.classList.remove("pulse");
+  // seven.classList.remove("pulse");
+  // eight.classList.remove("pulse");
+  // nine.classList.remove("pulse");
+  // refreshButton.classList.remove("pulse");
+
+  // one.classList.remove("delpink");
+  // two.classList.remove("delpink");
+  // three.classList.remove("delpink");
+  // four.classList.remove("delpink");
+  // five.classList.remove("delpink");
+  // six.classList.remove("delpink");
+  // seven.classList.remove("delpink");
+  // eight.classList.remove("delpink");
+  // nine.classList.remove("delpink");
+  // four.classList.remove("invisible");
 }
